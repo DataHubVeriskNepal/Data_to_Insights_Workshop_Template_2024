086dd83a0f012ed0b1e0f12d20ab18b80d8b5dce
Rabin Dhamala <rabindhamala17@gmail.com>
2024-11-14 20:00:13 +0545

    move files to child directory, add s3_uploader util and initialize main

---
+++ Scraping/forex/scrape/main.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from scraper import ForexScraper
+
+def main():
+    # Load environment variables
+    load_dotenv()
+
+    # Initialize a ForexScraper instance and call the run method
+    scraper = ForexScraper()
+    scraper.run()
+
+if __name__ == '__main__':
+    main()
